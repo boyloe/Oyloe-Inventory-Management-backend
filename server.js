f15226624442a0435e68aa401807eebe01e0bf94
boyloe <boyloe@gmail.com>
2020-10-27 23:11:36 -0600

    Add get action to /products

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const dbName = 'siteInventory'
+const dbURL = 'mongodb://localhost:27017/animals'
+const cors = require('cors')
+
 
 const port = process.env.PORT||4000
 
-
-app.get('/', (request, response) => {
-    response.send('hello')
-})
-
-app.post('/products', (request, response) => {
-    console.log('this is the products page')
+MongoClient.connect(dbURL, {useUnifiedTopology: true }, (err, client) => {
+    if (err) return console.log(err)
+    const db = client.db(dbName)
+    
+    
+    app.use(cors())
+    app.use(bodyParser.json())
+    app.get('/products', (request, response) => {
+        db.collection('products').find().toArray((err, result) => {
+            if (err) throw err
+            response.json(result)
+            })   
+    })
 })
 
 app.listen(port)
